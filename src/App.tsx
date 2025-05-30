02df68364a86dd03e31e59884a70118ebf97f0cd
swethanandakumarvh <swetha.nandakumarvh@gmail.com>
2025-05-30 23:42:15 +0530

    Add React import to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
